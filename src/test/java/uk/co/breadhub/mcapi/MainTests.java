c4f3822b6bcbf1b9d34ba312e89e552476742ec5
braddevans <braddevans123@gmail.com>
2020-07-25 02:51:35 +0100

    cleanup

--- src/test/java/uk/co/breadhub/mcapi/MainTests.java
+++ src/test/java/uk/co/breadhub/mcapi/MainTests.java
@@ -1,5 +1,7 @@
 package uk.co.breadhub.mcapi;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,14 @@ class MainTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
     }
 
 }
